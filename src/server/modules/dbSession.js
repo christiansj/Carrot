8b15c966bf9c6b7840833cb6df6661a5822975ef
Christian <christiansj10@gmail.com>
2020-02-28 18:48:52 -0600

    added executeQuery and sendResults utility functions

--- src/server/modules/dbSession.js
+++ src/server/modules/dbSession.js
@@ -1,10 +1,32 @@
-const config = require('./../../../config/sql-signin');
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + './../../../.env' });
 
+var connection = require('mysql').createConnection({
+  host: 'localhost', user: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD, database: 'carrot',
+  multipleStatements: true
+});
 
- var connection = require('mysql').createConnection({
-   host: 'localhost', user: config.USERNAME, 
-   password: config.PASSWORD, database: 'bobsbooks',
-   multipleStatements: true
- });
+module.exports.connection = connection;
 
- module.exports.connection = connection;
+module.exports.executeQuery =  function(query, parameters = [], callback){
+  connection.query(query, parameters, function(err, result){
+    if(err){
+      callback(err,null);
+    }else{
+      callback(null, result);
+    }
+  });
+}
+
+module.exports.sendResults = function(err, results, response, isSendOne = false){
+  if(err){
+      response.send(400);
+  }else{
+    if(isSendOne){
+      response.json(results[0]);
+    }else{
+      response.json(results);
+    }
+  }
+}

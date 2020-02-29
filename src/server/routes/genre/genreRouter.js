8dd4348e5d69c5e5d9b61641d94be620012af39c
Christian <christiansj10@gmail.com>
2020-02-28 18:31:45 -0600

    genre get routes

--- src/server/routes/genre/genreRouter.js
+++ src/server/routes/genre/genreRouter.js
@@ -1,8 +1,30 @@
 const express = require("express"),
+genreScripts = require('./../../sql-scripts/genre'),
+{executeQuery} = require('./../../modules/dbSession'),
 router = express.Router();
 
-router.route("/")
-	
-router.get("/getAll", function(req,res,next){
-  
+router.get("/", function(req,res,next){
+    const query = genreScripts.databaseTable;
+    executeQuery(query, [], (err, results)=>{
+        if(err){
+            console.log(err);
+            res.send(400);
+        }else{
+            res.json(results);
+        }
+    });
 });
+
+router.get("/:id", (req, res)=>{
+    const query = genreScripts.byId;
+    const {id} = req.params;
+    executeQuery(query, [id], (err, results)=>{
+        if(err){
+            res.send(400);
+        }else{
+            res.json(results[0]);
+        }
+    });
+});
+
+module.exports = router;

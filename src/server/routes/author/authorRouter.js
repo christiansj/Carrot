e5c2b94731130935e55904a97f0f1c6be7a22300
Christian <christiansj10@gmail.com>
2020-02-28 19:28:33 -0600

    trimmed down current authorRouter

---
+++ src/server/routes/author/authorRouter.js
@@ -0,0 +1,17 @@
+const express = require("express"),
+router = express.Router(),
+authorScripts = require('./../../sql-scripts/author'),
+{executeQuery, sendResults} = require("./../../modules/dbSession");
+
+// GET author/books/:userId
+router.get("/books/:userId", (req, response)=> {
+	const query = authorScripts.booksFromAuthor;
+	const {userId} = req.params;
+
+	executeQuery(query, [userId], (err, results)=>{
+		sendResults(err, results, response);
+	});
+});
+
+
+module.exports = router;

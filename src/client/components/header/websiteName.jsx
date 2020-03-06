931f10ffbad76991423b51c1c2641003823f1314
Christian <christiansj10@gmail.com>
2020-03-06 15:47:53 -0600

    fixed router issue by removing Link

--- src/client/components/header/websiteName.jsx
+++ src/client/components/header/websiteName.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 const websiteName = (
-  <Link to="/home/" className="nav-link" style={{ color: "white" }}>
+  <a href="/home/" className="nav-link" style={{ color: "white" }}>
     <span className="app-name">
       <big>Carrot</big>
     </span>
-  </Link>
+  </a>
 );
 export default websiteName;

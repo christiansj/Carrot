3fb0c4df4137279eb03326c551ed4be5fb12f71f
Christian <christiansj10@gmail.com>
2020-03-09 16:50:51 -0500

    setting up for testing

--- src/client/util/index.js
+++ src/client/util/index.js
@@ -1,4 +1,7 @@
 import checkPropTypes from 'check-prop-types';
+import rootReducer from './../redux/reducers';
+import {applyMiddleware, createStore} from 'redux';
+import {middlewares} from './../redux/stores/test-store';
 
 export const findByTestAttr = (component, attr) => {
     const wrapper = component.find(`[data-test='${attr}']`);
@@ -12,4 +15,9 @@ export const checkProps = (component, expectedProps) => {
 
 export const isEmpty = (obj) =>{
     return !obj || Object.keys(obj).length === 0;
+}
+
+export const testStore = (initialState) => {
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 }

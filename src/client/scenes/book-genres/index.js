ecea52c54fa8e41a7c94c0f307174c3d4e4a6335
Christian <christiansj10@gmail.com>
2020-03-13 11:27:50 -0500

    genreBooks are now displayed evenly

--- src/client/scenes/book-genres/index.js
+++ src/client/scenes/book-genres/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GenreBookCover from './components/GenreBookCover';
 import ApiService from 'client/services/Api';
 import './genre-books.css';
-
+//TODO if no books are found, redirect to "We couldn't find what you were looking for page"
 class GenreBookView extends Component {
   state = { bookMatches: [] }
 
@@ -10,7 +10,6 @@ class GenreBookView extends Component {
     const { genreName } = this.props.match.params;
     new ApiService().execute("GET", `genre/books/${genreName}`)
       .then(res => this.setState({ bookMatches: res.data }))
-      .then(console.log(this.state.bookMatches))
       .catch(err => console.error(err.response.data))
   }
 
@@ -23,14 +22,23 @@ class GenreBookView extends Component {
         <hr />
         <div className="row">
           {
+            
             this.state.bookMatches.map((bookJSON, index) => {
               return GenreBookCover(bookJSON, bookJSON.title, index)
             })
           }
+          {evenOutRow(this.state.bookMatches.length)}
         </div>
       </div>
     );
   }
+}
+
+function evenOutRow(bookCnt){
+  if(bookCnt % 2 === 0){
+    return;
+  }
+  return <div className="col-6"/>
 }
 
 export default GenreBookView;

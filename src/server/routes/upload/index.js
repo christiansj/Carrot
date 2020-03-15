b1021772003aace9ce8b0cee4b6c1c498146ba45
Christian <christiansj10@gmail.com>
2020-03-15 13:28:17 -0500

    upload-router refactoring

--- src/server/routes/upload/index.js
+++ src/server/routes/upload/index.js
@@ -1,51 +1,35 @@
-//TODO clean this to accomodate for recent changes
-const express = require("express"),
-  multer = require("multer"),
-  router = express.Router(),
-  path = require("path"),
-  {bookFilePath} = require('./../../util/file-util');
-  ;
-  const destinationPath = __dirname+"/uploadx";
+const express = require("express");
+const  multer = require("multer");
+const  router = express.Router();
+const  path = require("path");
+const  { bookFilePath } = require('./../../util/file-util');
 
-
-// 
-
-router.post("/book-cover/:bookId",  (request, response)=>{
-  const {bookId} = request.params;
+router.post("/book-cover/:bookId", (request, response) => {
+  const { bookId } = request.params;
   var storage = multer.diskStorage({
-    destination: function(req, file, callback){
+    destination: function (req, file, callback) {
       callback(null, bookFilePath(bookId))
     },
     filename: function (req, file, callback) {
       callback(null, "0" + path.extname(file.originalname));
     }
   });
-  
+
   var upload = multer({
     storage: storage
-
   })
-// .single("bookCover");
-  .fields([{name: "bookCover", maxCount: 2}]);
-  console.log(JSON.stringify(request.body))
-  console.log(`req files ${JSON.stringify(request.files)}`)
-    upload(request, response, function(err){
-      if(err instanceof multer.MulterError){
-        console.log("mutler err" + err);
-      
-      }else if(err){
-        console.log(err)
-        
-      
-      }
-      console.log();
-      console.log(`req files ${JSON.stringify(request.files)}`)
-      response.send("ok")
-    })
-    
-   
-    
-    
+    .fields([{ name: "bookCover", maxCount: 2 }]);
+
+  upload(request, response, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log("mutler err" + err);
+
+    } else if (err) {
+      console.log(err)
+
+    }
+    response.send("ok")
+  })
 });
 
 module.exports = router;

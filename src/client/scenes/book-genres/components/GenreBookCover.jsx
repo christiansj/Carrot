08b980aaaeb25d9a98115105d402e1a8d123a779
Christian <christiansj10@gmail.com>
2020-03-15 14:29:06 -0500

    genrebookcover now display book covers

--- src/client/scenes/book-genres/components/GenreBookCover.jsx
+++ src/client/scenes/book-genres/components/GenreBookCover.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-/**
- * 
- * @param {JSON} bookJSON 
- * @param {string} bookTitle 
- */
-const GenreBookCover = (bookJSON, bookTitle, index) => (
-  <div className="genre-book-cover container col-12 col-md-6" key={`book-cover-${index}`}>
-    <img src={"http://localhost:8080/" + bookJSON.imagePath + "0.jpg"}
-      height="250" width="150" 
-      className="genre-book-cover image" alt={`'${bookTitle}' cover`}/>
-    <span className="genre-book-cover details" style={{ textAlign: 'left !important' }}>
-      <p>
-        <a href={`/book/${bookJSON.bookId}-${bookTitle}`} style={{ textAlign: 'left !important' }}>
-          {bookJSON.title}
-        </a>
-        <br />
-        {bookJSON.description}
-      </p>
-    </span>
-  </div>
-)
+import bookCover from 'client/components/book-cover';
+
+const GenreBookCover = (bookJSON, bookTitle, index) => {
+  const bookCoverConfig = {
+    book: bookJSON,
+    height: 250,
+    width: 150,
+  }
+
+  return (
+    <div className="genre-book-cover container col-12 col-md-6" key={`book-cover-${index}`}>
+
+      {bookCover(bookCoverConfig)}
+      <span className="genre-book-cover details">
+        <p>
+          <a href={`/book/${bookJSON.bookId}-${bookTitle}`}>
+            {bookJSON.title}
+          </a>
+          <br />
+          {bookJSON.description}
+        </p>
+      </span>
+    </div>
+  )
+}
 
 export default GenreBookCover;

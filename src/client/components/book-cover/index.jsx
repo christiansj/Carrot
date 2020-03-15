9c216c85e7758e1f846dc550d7e6cd7c30b0a1b0
Christian <christiansj10@gmail.com>
2020-03-15 13:33:00 -0500

    book-covers now link to respective book detail page

--- src/client/components/book-cover/index.jsx
+++ src/client/components/book-cover/index.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-const renderPNG = (pngCheck, bookJSON, e) =>{
-  if(pngCheck) return;
-  const {bookId, folderHash} = bookJSON;
-  e.target.src = `http://localhost:8080/static/uploads/books/${folderHash}/${bookId}/0.png`;
+const renderPNG = (pngCheck, bookJSON, e) => {
+    if (pngCheck) return;
+    const { bookId, folderHash } = bookJSON;
+    e.target.src = `http://localhost:8080/static/uploads/books/${folderHash}/${bookId}/0.png`;
 }
 
 const bookCover = (props = {}) => {
-  const {book, height, width, className} = props;
-  const {bookId, title, folderHash} = book;
-  var pngCheck = false;
- 
-  const imagePath = `http://localhost:8080/static/uploads/books/${folderHash}/${bookId}/0.jpg`;
+    const { book, height, width, className } = props;
+    const { bookId, title, folderHash } = book;
+    var pngCheck = false;
 
-  return (
-    <img src={imagePath} height={height} width={width} alt={title}
-      className={className}
-      onError={(e) => {
-        renderPNG(pngCheck, book, e);
-        pngCheck = true;
-      }}
-    />
-  )
+    const imagePath = `http://localhost:8080/static/uploads/books/${folderHash}/${bookId}/0.jpg`;
+
+    return (
+        <a href={`/book/${bookId}-${title}`}>
+            <img src={imagePath} height={height} width={width} alt={title}
+                className={className}
+                onError={(e) => {
+                    renderPNG(pngCheck, book, e);
+                    pngCheck = true;
+                }}
+            />
+        </a>
+
+    )
 }
 export default bookCover;
